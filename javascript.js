abf5b1cad448ac5d3ae904ea0940f71363ec8061
Youssef <ybelalmarzouk@gmail.com>
2024-02-04 03:42:01 +0200

    feat:add find location function

--- javascript.js
+++ javascript.js
@@ -7,22 +7,28 @@ async function getIp() {
 	var dataIp = await response.json();
 	var ip = dataIp.query;
 	var location = dataIp.city;
-	var timeZone = dataIp.timeZone
-	var isp = dataIp.isp
-	getLocation(ip)
+	var timeZone = dataIp.timezone;
+	var isp = dataIp.isp;
 	locationInfo(ip, location, timeZone, isp)
 }
 
-async function getLocation(ip) {
-	const response = await fetch("https://api.geoapify.com/v1/ipinfo?apiKey=" + ip)
+function ipInput() {
+	var inputIp = document.querySelector("input").value;
+	getLocation(inputIp);
+}
+
+async function getLocation(inputIp) {
+	const response = await fetch("https://api.geoapify.com/v1/ipinfo?apiKey=" + inputIp)
 	var dataLocation = await response.json();
-	console.log(ip)
+	var ipLongitude = dataLocation.location.longitude;
+	var ipLatitude = dataLocation.location.latitude;
+	console.log(inputIp);
+	console.log(ipLongitude);
+	console.log(ipLatitude);
+	findLocation(ipLongitude, ipLatitude)
 }
 
 getIp()
-
-
-function ipInput() { }
 
 function locationInfo(ip, location, timeZone, isp) {
 	document.querySelector(".location-info-container").innerHTML = `
@@ -69,4 +75,20 @@ if (navigator.geolocation) {
 
 	}
 	);
+}
+
+function findLocation(longitude, latitude) {
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition((position) => {
+			var Latitude = position.coords.latitude;
+			var Longitude = position.coords.longitude;
+			var mark = document.querySelector(".location-icon");
+			var mapContainer = document.querySelector(".map-container");
+			var map = ` <iframe src="https://www.openstreetmap.org/export/embed.html?bbox=${longitude},${latitude}&;layer=mapnik"></iframe>`
+			mapContainer.innerHTML = map;
+
+		}
+		);
+	}
+
 }
